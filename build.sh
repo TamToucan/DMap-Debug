c0725696f13296e0c300bab6e8ab9f2a102e7e79
tam <tamtoucan@gmail.com>
2025-08-09 13:32:52 +0100

    move build.sh

---
+++ build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+cp test/GRID.txt .
+g++ -o testMain \
+-I test \
+-I src/ \
+-I lib/Util \
+-I lib/MathStuff/ \
+-I lib/Algo/ \
+-I lib/Stuff/ \
+test/testMain.cpp \
+test/vector2.cpp \
+test/vector2i.cpp \
+src/AbstractMST.cpp \
+src/FlowField.cpp \
+src/GDDistanceMap.cpp \
+src/GridToGraph.cpp \
+src/GridTypes.cpp \
+src/Router.cpp \
+src/Routing.cpp \
+src/WallDistanceGrid.cpp \
+lib/Stuff/TGA.cpp \
+lib/Algo/ZSThinning.cpp 
